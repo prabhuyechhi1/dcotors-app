8585431e358bb9b5d0e85b102d2437958cdc99b9
Allamaprabhu Yechhi <allamaprabhu_yechhi@hermanmiller.com>
2024-10-10 10:23:19 +0530

    Initial code push

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import NavBar from './components/NavBar'
+
+import './index.css';
+
+function App() {
+  return (
+    <div>
+      <NavBar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Add this line to specify the source files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
